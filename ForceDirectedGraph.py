66c7d6d2fc7530dbbd6f913cb73db8c128e44c4b
MxFxM <mxfxmmueller@gmail.com>
2022-03-25 23:51:23 +0100

    tests with force directed graph library

---
+++ ForceDirectedGraph.py
@@ -0,0 +1,27 @@
+# other imports
+from operator import indexOf
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import the package
+import forcelayout as fl
+
+# Need a dataset to visualise
+dataset = np.array([[2, 1, 0],
+                    [2, 1, 1], 
+                    [3, 2, 1]])
+
+names = ["first",
+         "second",
+         "third"]
+
+def annotate_points(node, i):
+    return names[i]
+
+# Need to use the brute force algorithm on a dataset this small
+# (not recommended for larger datasets)
+layout = fl.draw_spring_layout_animated(dataset=dataset,
+                                        algorithm=fl.SpringForce,
+                                        annotate=annotate_points)
+
+plt.show()
