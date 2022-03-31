e3dbd252dc946e72096fc01fea8807da9df27500
Max-Felix Mueller <max-felix.mueller@egoproducts.com>
2022-03-31 13:07:24 +0200

    more complex scoring

--- DetectSimilarities.py
+++ DetectSimilarities.py
@@ -11,7 +11,10 @@ matrix = []
 for line1 in lines:
     scores = []
     for line2 in lines:
-        scores.append(fuzz.token_sort_ratio(line1, line2))
+        tokenscore = fuzz.token_sort_ratio(line1, line2) / 100
+        ratioscore = fuzz.ratio(line1, line2) / 100
+        score = (tokenscore + ratioscore) / 2
+        scores.append(score)
     matrix.append(scores)
 
 with open("scores.csv", 'w+') as scorefile:
